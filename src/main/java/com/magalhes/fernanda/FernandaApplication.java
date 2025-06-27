d8401fa7bd486d514fe86b9aa0302e164fe92dd4
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2025-06-27 12:08:20 -0300

    m

---
+++ src/main/java/com/magalhes/fernanda/FernandaApplication.java
@@ -0,0 +1,13 @@
+package com.magalhes.fernanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magalhes/fernanda/FernandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalhes.fernanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FernandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
